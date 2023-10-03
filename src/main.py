075dcd98160d14122fd9c73b0a9c932aff994468
Swarms30 <samtoner30@gmail.com>
2023-10-03 11:17:36 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-#Credit to Max Talwar, Sam Engel, and Sam Warman for this code.
+#Click the run button once the csv files are placed into the "to be processed" folder. Once the program is done running, the output XLSX files will be in the "processed" folder
 
 import fileEditor
 import uploadToDrive
